80e1bae4b5fd6acbddafb03d01c1567b36b4bff3
nputans0.la <nputans0.la@tet.lv>
2020-07-06 12:10:39 +0300

    Added entity BlogPost and Category. Created Category Admin Interface

---
+++ code/src/Yoda/EventBundle/Admin/CategoryAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Yoda\EventBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class CategoryAdmin extends Admin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper->add('name', 'text');
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('name');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('name');
+    }
+}
