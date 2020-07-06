5264834ec4c5f3e23989bf918f0a64a9e0dcfe0f
nputans0.la <nputans0.la@tet.lv>
2020-07-06 13:58:24 +0300

    Added Form View and List View.

--- code/src/Yoda/EventBundle/Admin/BlogPostAdmin.php
+++ code/src/Yoda/EventBundle/Admin/BlogPostAdmin.php
@@ -2,16 +2,18 @@
 
 namespace Yoda\EventBundle\Admin;
 
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Yoda\EventBundle\Entity\BlogPost;
 use Yoda\EventBundle\Entity\Category;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 
-class BlogPostAdmin extends AbstractAdmin
+final class BlogPostAdmin extends AbstractAdmin
 {
 
-    protected function configureFormFields(FormMapper $formMapper)
+    protected function configureFormFields(FormMapper $formMapper) //builds form
     {
         $formMapper // creates form
             ->tab('Post') // creates tab 'Post'
@@ -32,9 +34,25 @@ class BlogPostAdmin extends AbstractAdmin
         ;
     }
 
-    protected function configureListFields(ListMapper $listMapper)
+    protected function configureListFields(ListMapper $listMapper) // builds list field
     {
-        // ... configure $listMapper
+        $listMapper
+            ->addIdentifier('title') // shows title
+            ->add('category.name') // shows category name
+            ->add('draft') // shows draft status
+            ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper) // builds filter/search option
+    {
+        $datagridMapper
+            ->add('title') // search by 'title'
+            ->add('category', null, [], EntityType::class, [ // search by 'category'
+                'class' => Category::class,
+                'choice_label' => 'name',
+            ])
+            ;
+
     }
 
     public function toString($object)
